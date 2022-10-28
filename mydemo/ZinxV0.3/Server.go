e12579547e3d09345a88509f4b4d2f1c4e2add64
但求一败 <qq2368269411@163.com>
2022-10-28 17:40:29 +0800

    zinx v0.3

--- mydemo/ZinxV0.3/Server.go
+++ mydemo/ZinxV0.3/Server.go
@@ -12,7 +12,7 @@ type PingRouter struct {
 
 func (this *PingRouter) PreHandle(request ziface.IRequest) {
 	fmt.Println("Call Router PreHandle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping..."))
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping...\n"))
 	if err != nil {
 		fmt.Println("call back before ping error")
 	}
@@ -20,7 +20,7 @@ func (this *PingRouter) PreHandle(request ziface.IRequest) {
 
 func (this *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call Router Handle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping"))
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping\n"))
 	if err != nil {
 		fmt.Println("call back ping...ping...ping error")
 	}
@@ -28,7 +28,7 @@ func (this *PingRouter) Handle(request ziface.IRequest) {
 
 func (this *PingRouter) PostHandle(request ziface.IRequest) {
 	fmt.Println("Call Router PostHandle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("after ping"))
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("after ping\n"))
 	if err != nil {
 		fmt.Println("call back after ping error")
 	}
